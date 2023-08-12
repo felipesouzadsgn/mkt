c35e770bba5eab90f21d7eb6c3e4987eda5a43de
Dragon Sound <dragonsound@Dragons-MacBook-Pro.local>
2023-08-12 15:55:59 -0300

    Update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,29 +8,8 @@ export default function Home() {
         <p className="text-orange-300">REGIAO CENTRAL-SP</p>
       </div>
       <section className="w-full max-w-7xl flex justify-between border h-96">
-        <div className="flex-1 border p-3">
-          <div className="w-full h-8 p-3">
-            <h3>USO DOS DADOS</h3>
-          </div>
-          <div className="w-full h-8 p-3">
-            <h3>PROJETO</h3>
-          </div>
-        </div>
-        <div className="flex-1 border">
-          <h3>DISTRIBUICAO</h3>
-        </div>
-        <div className="flex-1 border">
-          <h3>GERENTE</h3>
-        </div>
-        <div className="flex-1 border">
-          <h3>DISTRIBUICAO</h3>
-        </div>
-        <div className="flex-1 border">
-          <h3>STATUS</h3>
-        </div>
-
       </section>
-      <Link href="/dashboard">Dashboard</Link>
+      <Link href="/dashboard">SAIBA MAIS</Link>
     </main>
   )
 }
