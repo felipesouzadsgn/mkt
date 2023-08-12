2894f555da5db55138ff6d7a8b1861e622479536
Dragon Sound <dragonsound@Dragons-MacBook-Pro.local>
2023-08-12 15:36:34 -0300

    Initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <main>
+      <h1 className="text-3xl">Dashboard</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas voluptatem aut sunt veritatis autem illum vero iste nostrum in dignissimos quaerat, minus voluptatibus numquam, reiciendis qui nemo adipisci iusto aliquid!</p>
+    </main>
+  )
+}
